0489db497b248971c7921b6c4f6a2ad2c2659fc4
apayne97 <alex.payne@choderalab.org>
2026-03-24 10:46:21 -0400

    add protein rmsd analysis

---
+++ nextflow_workflows/02_dataset_analysis/nextflow_submission.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=dataset_anaysis
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=128:00:00
+#SBATCH --output=logs/dataset_analysis%j.out
+#SBATCH --error=logs/dataset_analysis_%j.err
+
+source ~/.bashrc
+source ~/.nextflowrc
+nextflow 00_dataset_analysis.nf -entry PROTEIN_RMSD_ANALYSIS -resume
